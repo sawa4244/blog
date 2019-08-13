c56ea3391b61f4d20b3d004f14c171213f96a870
sawa4244 <kolyada.s@bk.ru>
2019-08-13 04:40:48 +0300

    Changed the HTML for the site.

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def test(request):
-    return HttpResponse ("<h2>Главная{c}</h2>")
+    return render (request, 'index.html', {})
 
 def main(request):
     return HttpResponse("<h1>Это мейн</h1>")
